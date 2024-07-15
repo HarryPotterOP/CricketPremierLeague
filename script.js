c59a871df1aacce30b82f428e3a3122df3f8e1f9
HarryPotterOP <nevroy07@gmail.com>
2024-07-15 18:02:30 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+const scroll = new LocomotiveScroll({
+    el: document.querySelector('#main'),
+    smooth: true
+});
+
+
+function loaderAnimation() {
+    var loader = document.querySelector("#loader")
+    setTimeout(function () {
+        loader.style.top = "-100%"
+    }, 4200)
+}
+
+loaderAnimation()
