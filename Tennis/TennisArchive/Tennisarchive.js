794dcbbd0b0df021c687eccdf8fac3ff54f1e93b
HarryPotterOP <nevroy07@gmail.com>
2026-04-16 11:21:53 +0530

    Delete Tennis directory

--- Tennis/TennisArchive/Tennisarchive.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-  // Animate player cards in view
-  document.querySelectorAll('.player-card').forEach((card, i) => {
-    card.style.animationDelay = (i * 0.2) + 's';
-  });
-});
